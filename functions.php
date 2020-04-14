f04a88b6531a2ceaf049250ef18cc7078bf8c450
mmhayman <mmhayman@svsu.edu>
2020-04-14 15:29:45 -0700

    template finished

--- functions.php
+++ functions.php
@@ -1,9 +1,7 @@
 <?php
 
-function load_stylesheets() 
-
+function load_stylesheets()
 {
-
 
     wp_register_style('bootstrap', get_template_directory_uri() . '/css/bootstrap.min.css', array(), false, 'all');
     wp_enqueue_style('bootstrap');
@@ -14,12 +12,38 @@ function load_stylesheets()
 }
 add_action('wp_enqueue_scripts', 'load_stylesheets');
 
-
-function loadjs() 
+function include_jquery()
 {
 
+    wp_deregister_script('jquery');
 
-    wp_enqueue_scripts('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
+    wp_enqueue_script('jquery', get_template_directory_uri() . '/js/jquery-3.5.0.min.js', '', 1, true);
+
+    add_action('wp_enqueue_scripts', 'jquery');
+
+}
+add_action('wp_enqueue_scripts', 'include_jquery');
+
+function loadjs()
+{
+
+    wp_register_script('customjs', get_template_directory_uri() . '/js/scripts.js', '', 1, true);
     wp_enqueue_script('customjs');
+
 }
 add_action('wp_enqueue_scripts', 'loadjs');
+
+add_theme_support('menus');
+
+add_theme_support('post-thumbnails');
+
+register_nav_menus(
+    array(
+
+        'top-menu' => __('Top Menu', 'theme'),
+        'footer-menu' => __('Footer Menu', 'theme'),
+    )
+);
+
+add_image_size('smallest', 300, 300, true);
+add_image_size('largest', 800, 800, true);
--- header.php
+++ header.php
@@ -8,3 +8,15 @@
 </head>
 
 <body <?php body_class();?>>
+
+<header class="sticky-top">
+
+        <div class="container">
+        <?php wp_nav_menu(
+    array(
+        'theme_location' => 'top-menu',
+        'menu_class' => 'navigation',
+    )
+);?>
+        </div>
+</header>
